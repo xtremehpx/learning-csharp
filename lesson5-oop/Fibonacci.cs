93299292767c709baa2158e0812c46372d65b2cc
Paul <hui.p.xiong@gmail.com>
2023-08-02 19:06:02 -0700

    lesson 5 class notes

---
+++ lesson5-oop/Fibonacci.cs
@@ -0,0 +1,78 @@
+﻿namespace lesson5_oop
+{
+    public class Fibonacci
+    {
+        // constructor
+        public Fibonacci()
+        {
+            // default constructor
+        }
+
+        public Fibonacci(int num)
+        {
+            Number = num;                
+        }
+
+        // simple getter / setter
+        // public int MyProperty { get; set; }
+
+        private int _number = 0;
+        public int Number {
+            get
+            {
+                
+                return _number;
+            }
+
+            set
+            {
+                if (value < 10)
+                {
+                    _number = 0;
+                }
+                else
+                {
+                    _number = value * 10;
+                }
+            }
+        }
+
+
+        
+
+        public const int F0 = 0;
+        public const int F1 = 1;
+
+        public int Calculate()
+        {
+
+            int f0 = 0;
+            int f1 = 1;
+            int f = 0;
+
+            for (int a = 2; a < Number; a = a + 1)
+            {
+                f = f0 + f1;
+                f0 = f1;
+                f1 = f;
+            }
+
+            return f;
+        }
+
+
+        // unit test
+
+        //
+
+
+
+        // OOP other topics
+
+        // inheritance :: child class and parent class relation
+
+        // abstract class
+
+        // interface class 
+    }
+}
---
+++ lesson5-oopTests/FibonacciTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lesson5_oop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5_oop.Tests
+{
+    [TestClass()]
+    public class FibonacciTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+
+            // check if Fibonacci class calculation is correct for Number = 10
+            int expected_return = 55;
+
+            int actual_return = 0;
+
+
+            // Chapter 5:: exercise 
+            // actual_return ==??? how to get it?
+
+
+
+
+            Assert.AreEqual(expected_return, actual_return);
+        }
+    }
+}
