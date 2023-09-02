3ab503fdb6cb46c9831efebbb6631f2276e1bfe3
xtremehpx <hui.p.xiong@hotmail.com>
2023-09-01 23:27:26 -0700

    add lesson 10 content

---
+++ lesson10-modularizing/Fibonacci.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace lesson10_modularizing
+{
+    public class Fibonacci
+    {
+        // constructor
+        public Fibonacci()
+        {
+            // default constructor
+        }
+
+        public Fibonacci(int num)
+        {
+            Number = num;                
+        }
+
+        // simple getter / setter
+        // public int MyProperty { get; set; }
+
+        private int _number = 0;
+        public int Number {
+            get
+            {
+                
+                return _number;
+            }
+
+            set
+            {
+                if (value < 10)
+                {
+                    _number = 0;
+                }
+                else
+                {
+                    _number = value * 10;
+                }
+            }
+        }
+
+
+        
+
+        public const int F0 = 0;
+        public const int F1 = 1;
+
+        public int Calculate()
+        {
+
+            int f0 = 0;
+            int f1 = 1;
+            int f = 0;
+
+            for (int a = 2; a < Number; a = a + 1)
+            {
+                f = f0 + f1;
+                f0 = f1;
+                f1 = f;
+            }
+
+            return f;
+        }
+
+
+        // unit test
+
+        //
+
+
+
+        // OOP other topics
+
+        // inheritance :: child class and parent class relation
+
+        // abstract class
+
+        // interface class 
+
+
+        public int CalculateRecursion(int n) 
+        {
+            if(n==0) return 0;
+            if(n==1) return 1;
+
+            Debug.WriteLine( "Calculate " + n );
+
+            return CalculateRecursion(n - 1) + CalculateRecursion(n - 2);
+        }
+
+        // key - value pair, hash map
+        // Algorithm efficiency:: Big-O O(n), O(n^2)
+        // Hash Map / Hash Table / Dictionary :: use key value - O(1) 
+        Dictionary<int, int> dict = new Dictionary<int, int>(); 
+        public int CalculateDpp(int n)
+        {
+            if(n==0) return 0;
+            if(n==1) return 1;
+
+            Debug.WriteLine("Calculate " + n);
+
+            var fn1 = dict.ContainsKey(n - 1) ? dict[n-1] : CalculateDpp(n - 1);
+            var fn0 = dict.ContainsKey(n - 2) ? dict[n - 2] : CalculateDpp(n - 2);
+
+            dict[n] = fn1 + fn0;
+
+            return dict[n];
+        }
+    }
+}
---
+++ lesson10-modularizing/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson10_modularizing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //class2 7/22/23 S
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+            //class2
+            //class3 7/26/23 W
+            //while loops
+
+            int a = 10;
+            do
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a = a + 1;
+            }
+            while (a < 20);
+
+
+            while (a < 20)
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+
+                if (a > 15)
+                {
+                    break;
+                }
+            }
+
+            //for loops
+            for (int aa = 10; aa < 20; aa = aa + 1)
+            {
+                Console.WriteLine("value of aa: {0}", aa);
+            }
+
+            //class3
+            //class4 7/29/23 S
+            int n = 10;
+            int m = 0;
+            m = n + 1;
+
+            // use normal Function inside local program
+            var f = Fibonnaci(m);
+            Console.WriteLine("Fibonacci number is {0}", f);
+
+
+            // use OOP class
+
+            // default constructor
+            //create a new Object of Fibonacci
+            Fibonacci fb = new Fibonacci();
+            Console.WriteLine("Default value of Number: " + fb.Number);
+            Console.WriteLine(string.Format("using string.Format: :Default value of Number: {0}, {1}", fb.Number, 2));
+
+            // access member Number
+            // assign value
+            fb.Number = 10;
+            Console.WriteLine("Assigned value of Number: " + fb.Number);
+
+            Console.WriteLine("Dynamic Programming");
+            fb.CalculateDpp(10);
+
+            Console.WriteLine("Recursion");
+            fb.CalculateRecursion(10);
+
+            // parametric constructor
+            fb = new Fibonacci(13);
+            Console.WriteLine("Default value of Number: " + fb.Number);
+
+
+            //class4 7/29/23 S
+            Console.ReadLine();
+        }
+
+        // for, while, recursive, dynamic programming
+        public static int Fibonnaci(int m)
+        {
+            int f0 = 0;
+            int f1 = 1;
+            int f = 0;
+
+            for (int a = 2; a < m; a = a + 1)
+            {
+                f = f0 + f1;
+                f0 = f1;
+                f1 = f;
+            }
+
+            return f;
+        }
+    }
+}
